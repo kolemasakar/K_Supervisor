20e6051be4a826e934111b0fe83cf571627a9c6d
kolemasakar <kolemasakar@gmail.com>
2026-09-13 23:26:00 +0300

    Phase 14: export structured approval coordinator

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -3,10 +3,13 @@ from models.workflow import WorkflowDefinition, WorkflowNode, WorkflowNodeType
 from .approval import ApprovalRequester, HumanInterventionApprovalRequester
 from .contracts import WorkflowExecutionResult, WorkflowExecutionStatus
 from .engine import WorkflowEngine, WorkflowRuntimeError
+from .structured_approval import StructuredApprovalCoordinator, StructuredApprovalRecord
 
 __all__ = [
     "ApprovalRequester",
     "HumanInterventionApprovalRequester",
+    "StructuredApprovalCoordinator",
+    "StructuredApprovalRecord",
     "WorkflowDefinition",
     "WorkflowEngine",
     "WorkflowExecutionResult",
