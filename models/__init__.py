cbee43e921b19202652b501461f0b1dcdf068906
kolemasakar <kolemasakar@gmail.com>
2026-09-13 16:23:35 +0300

    Phase 1: expose core contract models

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .project import Project, ProjectSpec
+from .task import Task, WorkflowRun
+from .capability import CapabilityDescriptor, CapabilityRequirement
+from .agent import AgentDescriptor, AgentRunRequest, AgentRunResult
+from .intervention import HumanActionRequest, NotificationEvent
+from .release import Release
