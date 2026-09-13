5f2cfbc58f560dc43b7743d18d99202328145d68
kolemasakar <kolemasakar@gmail.com>
2026-09-13 21:49:28 +0300

    Phase 10: export integration contracts

---
+++ integrations/__init__.py
@@ -0,0 +1,8 @@
+from .contracts import AvailabilityReport, AvailabilityState, DependencyRequirement, IntegrationKind
+
+__all__ = [
+    "AvailabilityReport",
+    "AvailabilityState",
+    "DependencyRequirement",
+    "IntegrationKind",
+]
