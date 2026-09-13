8bf3a782f616369ad793531c87af3fef5b8126de
kolemasakar <kolemasakar@gmail.com>
2026-09-13 21:50:21 +0300

    Phase 10: export generic Provider contracts

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,1 +1,18 @@
+from .contracts import (
+    ModelProfile,
+    Provider,
+    ProviderDescriptor,
+    ProviderRequest,
+    ProviderResponse,
+)
 from .email import EmailProvider, OutboundEmail
+
+__all__ = [
+    "Provider",
+    "ProviderDescriptor",
+    "ProviderRequest",
+    "ProviderResponse",
+    "ModelProfile",
+    "EmailProvider",
+    "OutboundEmail",
+]
