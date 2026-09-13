13ee5b12c03a0840b74c3cbaadfabbeb2c408cdc
kolemasakar <kolemasakar@gmail.com>
2026-09-13 16:44:26 +0300

    Phase 2: export ArtifactReference

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,6 @@ from .capability import CapabilityDescriptor, CapabilityRequirement
 from .agent import AgentDescriptor, AgentRunRequest, AgentRunResult
 from .intervention import HumanActionRequest, NotificationEvent
 from .release import Release
+from .artifact import ArtifactReference
 from .lifecycle import ProjectLifecycleTransition
 from .operational import ProjectOperationalTransition
