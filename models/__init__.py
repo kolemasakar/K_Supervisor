e5b13e3f6384d5ad3d94030316a8caa336a9cc47
kolemasakar <kolemasakar@gmail.com>
2026-09-13 16:25:47 +0300

    Phase 1: export transition models

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,5 @@ from .capability import CapabilityDescriptor, CapabilityRequirement
 from .agent import AgentDescriptor, AgentRunRequest, AgentRunResult
 from .intervention import HumanActionRequest, NotificationEvent
 from .release import Release
+from .lifecycle import ProjectLifecycleTransition
+from .operational import ProjectOperationalTransition
