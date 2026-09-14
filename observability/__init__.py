69101476ee84438cdf3b1a912dbec7694c325d6b
kolemasakar <kolemasakar@gmail.com>
2026-09-14 03:17:21 +0300

    Phase 15: export observability integrations

--- observability/__init__.py
+++ observability/__init__.py
@@ -1,14 +1,20 @@
 from .audit import record_audit
 from .failure_injection import DeterministicFailureInjector, InjectedFailure
+from .kernel import ObservableSupervisorKernel
 from .metrics import MetricsCollector
 from .reliability import ReliabilityReport, ReliabilityValidator
+from .release import ObservableReleaseReadinessChecker
 from .release_validation import record_release_validation
 from .routing import record_routing
+from .timeline import AuditTimeline
 
 __all__ = [
+    "AuditTimeline",
     "DeterministicFailureInjector",
     "InjectedFailure",
     "MetricsCollector",
+    "ObservableReleaseReadinessChecker",
+    "ObservableSupervisorKernel",
     "ReliabilityReport",
     "ReliabilityValidator",
     "record_audit",
