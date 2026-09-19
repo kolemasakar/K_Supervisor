8aabac262d9838a9396cf0acc1851cb57b8da7ca
kolemasakar <kolemasakar@gmail.com>
2026-09-19 14:36:30 +0300

    Export governed model-backed agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,7 +1,9 @@
 from .catalog import reference_agent_entries, register_reference_agents
+from .model_backed import ModelBackedAgent
 from .reference import CriticAgent, DataAnalysisAgent, FactCheckAgent, ReportAgent, ResearchAgent
 
 __all__ = [
+    "ModelBackedAgent",
     "ResearchAgent",
     "CriticAgent",
     "ReportAgent",
