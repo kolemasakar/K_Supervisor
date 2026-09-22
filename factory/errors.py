9719c02afb22b66d8bddb2f34f26a6baec08c7ed
kolemasakar <kolemasakar@gmail.com>
2026-09-22 18:15:35 +0300

    phase4: distinguish governed repository blocks

--- factory/errors.py
+++ factory/errors.py
@@ -14,6 +14,10 @@ class RepositoryUnavailableError(ProjectFactoryError):
     pass
 
 
+class RepositoryGovernanceBlockedError(RepositoryUnavailableError):
+    pass
+
+
 class BootstrapBlockedError(ProjectFactoryError):
     def __init__(self, message: str, human_action_id: str | None = None):
         super().__init__(message)
